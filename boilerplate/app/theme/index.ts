ad377fe714d8047c8266ecd60e1e91e76072ee01
vserifsaglam <vahit.serif119@gmail.com>
2020-08-19 01:36:11 +0300

    added shopify restyle

--- boilerplate/app/theme/index.ts
+++ boilerplate/app/theme/index.ts
@@ -1,16 +1,63 @@
 import borderRadius from "./radius";
 import { spacing } from "./spacing";
-import colors from './color'
-import typography from './typography'
+import {BaseTheme, createTheme} from '@shopify/restyle'
+import color from './color'
 
-const theme = {
-  spacing,
-  colors,
-  borderRadius,
-  typography
+
+
+
+export interface GradientVariant {
+  colors ?: Array<string>,
+  locations ?: Array<number>,
+  start ?: {x: number, y: number},
+  end ?: {x: number, y: number},
 }
+
+export type Theme = BaseTheme & {
+  gradientVariants ?: {
+    [key: string]: GradientVariant
+  }
+};
+
+const theme : Theme = createTheme({
+  colors: {
+    success: "#42ef4a",
+    error: "#ff3d3d",
+    black: "#000",
+    white: "#fff",
+    ...color
+  },
+  cardVariants: {
+    primary: {},
+    secondary: {}
+  },
+  textVariants: {
+    primary: {},
+    secondary: {},
+  },
+  buttonVariants: {
+    primary: {},
+    secondary: {},
+
+  },
+  textInputVariants: {
+    primary: {},
+    secondary: {},
+    success: {},
+    error: {}
+  },
+  borderRadii: borderRadius,
+  spacing: spacing,
+  breakpoints: {
+    phone: 0,
+    tablet: 768,
+  },
+  gradientVariants: {
+    primary: {},
+    secondary: {},
+  }
+});
 
 
 export default theme;
-
 
