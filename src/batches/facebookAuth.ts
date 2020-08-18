3cfdd96d2cc14469f4c0300e38abc861d71d7e7e
vserifsaglam <vahit.serif119@gmail.com>
2020-08-18 15:56:47 +0300

    fixed some problems

--- src/batches/facebookAuth.ts
+++ src/batches/facebookAuth.ts
@@ -2,59 +2,72 @@ import { IgniteToolbox } from "../types"
 import { TemplateProps } from "../boilerplate"
 
 export default async function install(toolbox: IgniteToolbox, templateProps: TemplateProps) {
-  const { ignite } = toolbox
+  const { ignite, print } = toolbox
   const { name } = templateProps
 
   if (templateProps.useFacebookAuth) {
-    // insert facebook android client id  into strings.xml and AndroidManifest.xml
-    if (templateProps.facebookAndroidId && templateProps.facebookAndroidId !== "") {
-      ignite.patchInFile(
-        `${process.cwd()}/android/app/src/main/res/values/strings.xml`,
-        {
-          before: "</resources>",
-          insert: "    <string name=\"facebook_app_id\">" + templateProps.facebookAndroidId + " </string>\n" +
-            "    <string name=\"fb_login_protocol_scheme\">fb" + templateProps.facebookAndroidId + "</string>",
-        },
-      )
 
-      ignite.patchInFile(
-        `${process.cwd()}/android/app/src/main/res/AndroidManifest.xml`,
-        {
-          before: "<activity",
-          insert: "<meta-data android:name=\"com.facebook.sdk.ApplicationId\" android:value=\"@string/facebook_app_id\"/>",
-        },
-      )
+    const spinner = print.spin('Installing Facebook sdk')
+    try {
+      // insert facebook android client id  into strings.xml and AndroidManifest.xml
+      if (templateProps.facebookAndroidId && templateProps.facebookAndroidId !== "") {
+        ignite.patchInFile(
+          `${process.cwd()}/android/app/src/main/res/values/strings.xml`,
+          {
+            before: "</resources>",
+            insert: "    <string name=\"facebook_app_id\">" + templateProps.facebookAndroidId + " </string>\n" +
+              "    <string name=\"fb_login_protocol_scheme\">fb" + templateProps.facebookAndroidId + "</string>",
+          },
+        )
+
+        ignite.patchInFile(
+          `${process.cwd()}/android/app/src/main/res/AndroidManifest.xml`,
+          {
+            before: "<activity",
+            insert: "<meta-data android:name=\"com.facebook.sdk.ApplicationId\" android:value=\"@string/facebook_app_id\"/>",
+          },
+        )
+      }
+
+      // insert ios id into Info.plist
+      if (templateProps.facebookIosId && templateProps.facebookIosId !== "") {
+        ignite.patchInFile(
+          `${process.cwd()}/ios/${name}/Info.plist`,
+          {
+            after: "<dict>",
+            insert: "    <key>CFBundleURLTypes</key>\n" +
+              "    <array>\n" +
+              "      <dict>\n" +
+              "      <key>CFBundleURLSchemes</key>\n" +
+              "      <array>\n" +
+              "        <string>fb" + templateProps.facebookIosId + "</string>\n" +
+              "      </array>\n" +
+              "      </dict>\n" +
+              "    </array>\n" +
+              "    <key>FacebookAppID</key>\n" +
+              "    <string>" + templateProps.facebookIosId + "</string>\n" +
+              "    <key>FacebookDisplayName</key>\n" +
+              "    <string>" + name + "</string>\n" +
+              "    <key>LSApplicationQueriesSchemes</key>\n" +
+              "    <array>\n" +
+              "      <string>fbapi</string>\n" +
+              "      <string>fb-messenger-api</string>\n" +
+              "      <string>fbauth2</string>\n" +
+              "      <string>fbshareextension</string>\n" +
+              "    </array>",
+          },
+        )
+      }
+      spinner.succeed('Facebook sdk\'s installed')
+    }catch (e) {
+      spinner.fail('Facebook sdk could not install')
+
+      ignite.log(e)
+      // if something go wrong, ignore the rrror
     }
 
-    // insert ios id into Info.plist
-    if (templateProps.facebookIosId && templateProps.facebookIosId !== "") {
-      ignite.patchInFile(
-        `${process.cwd()}/ios/${name}/Info.plist`,
-        {
-          after: "<dict>",
-          insert: "    <key>CFBundleURLTypes</key>\n" +
-            "    <array>\n" +
-            "      <dict>\n" +
-            "      <key>CFBundleURLSchemes</key>\n" +
-            "      <array>\n" +
-            "        <string>fb" + templateProps.facebookIosId + "</string>\n" +
-            "      </array>\n" +
-            "      </dict>\n" +
-            "    </array>\n" +
-            "    <key>FacebookAppID</key>\n" +
-            "    <string>" + templateProps.facebookIosId + "</string>\n" +
-            "    <key>FacebookDisplayName</key>\n" +
-            "    <string>" + name + "</string>\n" +
-            "    <key>LSApplicationQueriesSchemes</key>\n" +
-            "    <array>\n" +
-            "      <string>fbapi</string>\n" +
-            "      <string>fb-messenger-api</string>\n" +
-            "      <string>fbauth2</string>\n" +
-            "      <string>fbshareextension</string>\n" +
-            "    </array>",
-        },
-      )
-    }
+
+
   }
 
   return true
