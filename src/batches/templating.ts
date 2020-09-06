08816798c4b458887896e1d53cabfeddb0eda3fa
vserifsaglam <vahit.serif119@gmail.com>
2020-09-06 10:00:07 +0300

    removed

--- src/batches/templating.ts
+++ src/batches/templating.ts
@@ -2,12 +2,11 @@ import { TemplateProps } from "../boilerplate"
 import { IgniteToolbox } from "../types"
 
 export default async function install(toolbox: IgniteToolbox, templateProps: TemplateProps) {
-  const {print, ignite} = toolbox
-  const spinner = print.spin('▸ generating files')
-
+  const { print, ignite } = toolbox
+  const spinner = print.spin("▸ generating files")
 
   const templates = [
-    { template: "index.js.ejs", target:  "index.js" },
+    { template: "index.js.ejs", target: "index.js" },
     { template: "README.md", target: "README.md" },
     { template: ".gitignore.ejs", target: ".gitignore" },
     { template: ".env.example", target: ".env" },
@@ -19,13 +18,13 @@ export default async function install(toolbox: IgniteToolbox, templateProps: Tem
     { template: "app/app.tsx.ejs", target: "app/app.tsx" },
     { template: "app/i18n/i18n.ts.ejs", target: "app/i18n/i18n.ts" },
     { template: "app/utils/storage/storage.ts.ejs", target: "app/utils/storage/storage.ts" },
-    { template: "app/utils/storage/storage.test.ts.ejs", target: "app/utils/storage/storage.test.ts", },
-    { template: "app/navigation/root-navigator.tsx.ejs", target: "app/navigation/root-navigator.tsx", },
-    { template: "app/navigation/primary-navigator.tsx.ejs", target: "app/navigation/primary-navigator.tsx", },
+    { template: "app/utils/storage/storage.test.ts.ejs", target: "app/utils/storage/storage.test.ts" },
+    { template: "app/navigation/root-navigator.tsx.ejs", target: "app/navigation/root-navigator.tsx" },
+    { template: "app/navigation/primary-navigator.tsx.ejs", target: "app/navigation/primary-navigator.tsx" },
     { template: "storybook/storybook.tsx.ejs", target: "storybook/storybook.tsx" },
     { template: "bin/postInstall", target: "bin/postInstall" },
     { template: "app/theme/color.ts.ejs", target: "app/theme/color.ts" },
-    {template: 'app/theme/index.ts.ejs', target: 'app/theme/index.ts'},
+    { template: "app/theme/index.ts.ejs", target: "app/theme/index.ts" },
     { template: "app/theme/typography.ts.ejs", target: "app/theme/typography.ts" },
     { template: "app/utils/social.ts.ejs", target: "app/utils/social.ts" },
   ]
@@ -36,8 +35,7 @@ export default async function install(toolbox: IgniteToolbox, templateProps: Tem
 
   await ignite.setIgniteConfig("navigation", "react-navigation")
 
-  spinner.succeed('files generated')
+  spinner.succeed("files generated")
 
   return true
-
 }
